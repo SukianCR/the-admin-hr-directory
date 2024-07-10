b6c562a664b352fdde1785628054cf6d285bf400
sukimixcr <sukimixcr@gmail.com>
2024-07-10 13:46:47 -0700

    Final product

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/the_acme_hr_directory_db"
 );
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3015;
 
 app.get("/api/departments", async (req, res, next) => {
   try {
@@ -42,6 +42,37 @@ app.post("/api/employees", async (req, res, next) => {
   }
 });
 
+app.put("/api/employees/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+        UPDATE employees
+        SET name=$1, department_id=$2, updated_at= now()
+        WHERE id=$3 RETURNING *
+      `;
+    const response = await client.query(SQL, [
+      req.body.name,
+      req.body.department_id,
+      req.params.id,
+    ]);
+    res.send(response.rows[0]);
+  } catch (ex) {
+    console.log(ex);
+  }
+});
+
+app.delete("/api/employees/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+        DELETE from employees
+        WHERE id = $1
+      `;
+    const response = await client.query(SQL, [req.params.id]);
+    res.sendStatus(204);
+  } catch (ex) {
+    console.log(ex);
+  }
+});
+
 const init = async () => {
   await client.connect();
   let SQL = `
@@ -61,13 +92,14 @@ const init = async () => {
   `;
   await client.query(SQL);
   SQL = `
-    INSERT INTO departments(name) values('AMERICAN BULL DOG');
-    INSERT INTO departments(name) values('Pekingese');
-    INSERT INTO departments(name) values('BEAGLE');
-    INSERT INTO departments(name) values('BLACK LAB');
-    INSERT INTO employees(name, age, department_id) values('BETTY', 9, (SELECT id from breeds WHERE breed='AMERICAN BULL DOG'));
-    INSERT INTO employees(name, age, department_id) values('LEO', 10, (SELECT id from breeds WHERE breed='BEAGLE'));
-    INSERT INTO employees(name, age, department_id) values('MOTI', 2, (SELECT id from breeds WHERE breed='Pekingese'));
+    INSERT INTO departments(name) values('food_beverages');
+    INSERT INTO departments(name) values('housekeeping');
+    INSERT INTO departments(name) values('events');
+    INSERT INTO departments(name) values('security');
+    INSERT INTO employees(name,department_id) values('Lucia', (SELECT id from departments WHERE name='food_beverages'));
+    INSERT INTO employees(name, department_id) values('Leo',  (SELECT id from departments WHERE name='events'));
+     INSERT INTO employees(name, department_id) values('Sara',  (SELECT id from departments WHERE name='events'));
+    INSERT INTO employees(name, department_id) values('Mario',  (SELECT id from departments WHERE name='security'));
   `;
   await client.query(SQL);
   app.listen(PORT, () => {
